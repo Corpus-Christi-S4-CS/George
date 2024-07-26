f7b747571e0101adfca44dabed3796877dbe42ee
George W <Georgew@gmail.comgwberczely@gmail.com>
2024-07-26 14:29:12 +0100

    skibidi

--- Day4/GUItrafficLight.py
+++ Day4/GUItrafficLight.py
@@ -14,34 +14,85 @@ c.pack()
 
 # positions starts from top left corner
 light_1 = c.create_rectangle(600,300,700,550,fill='black')
+light_2 = c.create_rectangle(450,300,550,550,fill='black')
 
 #Draw an Oval in the canvas
 radius = 25 
 x_center_top = 650
+x_center_left = 500
 y_center_top = 350
 y_center_middle = 425
 y_center_bottom = 500
+
 
 while True:
     
     top_light = c.create_oval(x_center_top-radius,y_center_top-radius,
                         x_center_top+radius,y_center_top+radius, fill = "red")
     top_light = c.create_oval(x_center_top-radius,y_center_middle-radius,
-                        x_center_top+radius,y_center_middle+radius, fill = "orange")
+                        x_center_top+radius,y_center_middle+radius, fill = "black")
     op_light = c.create_oval(x_center_top-radius,y_center_bottom-radius,
-                        x_center_top+radius,y_center_bottom+radius, fill = "green")
+                        x_center_top+radius,y_center_bottom+radius, fill = "black")
+    
+    top_light = c.create_oval(x_center_left-radius,y_center_top-radius,
+                        x_center_left+radius,y_center_top+radius, fill = "black")
+    top_light = c.create_oval(x_center_left-radius,y_center_middle-radius,
+                        x_center_left+radius,y_center_middle+radius, fill = "black")
+    top_light = c.create_oval(x_center_left-radius,y_center_bottom-radius,
+                        x_center_left+radius,y_center_bottom+radius, fill = "green")
+    
+    win.update()
+    time.sleep(3)
+
+    top_light = c.create_oval(x_center_top-radius,y_center_top-radius,
+                        x_center_top+radius,y_center_top+radius, fill = "red")
+    top_light = c.create_oval(x_center_top-radius,y_center_middle-radius,
+                        x_center_top+radius,y_center_middle+radius, fill = "black")
+    op_light = c.create_oval(x_center_top-radius,y_center_bottom-radius,
+                        x_center_top+radius,y_center_bottom+radius, fill = "black")
+    
+    top_light = c.create_oval(x_center_left-radius,y_center_top-radius,
+                        x_center_left+radius,y_center_top+radius, fill = "black")
+    top_light = c.create_oval(x_center_left-radius,y_center_middle-radius,
+                        x_center_left+radius,y_center_middle+radius, fill = "orange")
+    top_light = c.create_oval(x_center_left-radius,y_center_bottom-radius,
+                        x_center_left+radius,y_center_bottom+radius, fill = "black")
     
     win.update()
     time.sleep(1)
 
     top_light = c.create_oval(x_center_top-radius,y_center_top-radius,
-                        x_center_top+radius,y_center_top+radius, fill = "green")
+                        x_center_top+radius,y_center_top+radius, fill = "black")
+    top_light = c.create_oval(x_center_top-radius,y_center_middle-radius,
+                        x_center_top+radius,y_center_middle+radius, fill = "black")
+    op_light = c.create_oval(x_center_top-radius,y_center_bottom-radius,
+                        x_center_top+radius,y_center_bottom+radius, fill = "green")
+    
+    top_light = c.create_oval(x_center_left-radius,y_center_top-radius,
+                        x_center_left+radius,y_center_top+radius, fill = "red")
+    top_light = c.create_oval(x_center_left-radius,y_center_middle-radius,
+                        x_center_left+radius,y_center_middle+radius, fill = "black")
+    top_light = c.create_oval(x_center_left-radius,y_center_bottom-radius,
+                        x_center_left+radius,y_center_bottom+radius, fill = "black")
+    
+    win.update()
+    time.sleep(3)
+
+    top_light = c.create_oval(x_center_top-radius,y_center_top-radius,
+                        x_center_top+radius,y_center_top+radius, fill = "black")
     top_light = c.create_oval(x_center_top-radius,y_center_middle-radius,
                         x_center_top+radius,y_center_middle+radius, fill = "orange")
     op_light = c.create_oval(x_center_top-radius,y_center_bottom-radius,
-                        x_center_top+radius,y_center_bottom+radius, fill = "red")
+                        x_center_top+radius,y_center_bottom+radius, fill = "black")
+    
+    top_light = c.create_oval(x_center_left-radius,y_center_top-radius,
+                        x_center_left+radius,y_center_top+radius, fill = "red")
+    top_light = c.create_oval(x_center_left-radius,y_center_middle-radius,
+                        x_center_left+radius,y_center_middle+radius, fill = "black")
+    top_light = c.create_oval(x_center_left-radius,y_center_bottom-radius,
+                        x_center_left+radius,y_center_bottom+radius, fill = "black")
+    
     win.update()
-    print("GREEN")
     time.sleep(1)
 
 win.mainloop()
---
+++ Day5/Ex2.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+weights = np.array([1, 2, 3, 4, 5, 6])
+values = np.array([2, 4, 4, 5, 7, 9])
+
