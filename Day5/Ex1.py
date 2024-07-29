e56b9e1890638c1078339f3f3ee08f0aa260b0ee
George W <Georgew@gmail.comgwberczely@gmail.com>
2024-07-29 09:35:51 +0100

    rohan

--- Day5/Ex1.py
+++ Day5/Ex1.py
@@ -1,36 +1,52 @@
 import numpy as np
-import random
 
-def mutation(child):
+parent1 = np.array([0,0,1,0,1,0])
+parent2 = np.array([1,1,1,0,0,1])
 
-    mutationPoint = random.randint(0, len(parent1))
-
-    if child[mutationPoint] == 1:
-
-        child[mutationPoint] = 0
-
-    elif child[mutationPoint] == 0:
-
-        child[mutationPoint] = 1
-    
-    return child
+best_value = -1000
 
 def crossover(parent1, parent2):
+    cp = np.random.randint(0,len(parent1))
+    child1 = np.concatenate((parent1[:cp], parent2[cp:]))
+    child2 = np.concatenate((parent2[:cp], parent1[cp:]))
+    return child1, child2
 
-    crossoverPoint = random.randint(0, len(parent1))
+def mutation(child):
+    mp = np.random.randint(0,len(parent1))
+    if child[mp]==1:
+        child[mp]=0
+    elif child[mp]==0:
+        child[mp]=1
+    return child
 
-    child1 = np.concatenate((parent1[:crossoverPoint], parent2[crossoverPoint:]))
-    child2 = np.concatenate((parent2[:crossoverPoint], parent1[crossoverPoint:]))
+mutation_probability = 0.2
+weights = np.array([1,2,3,4,5,6])
+values =  np.array([2,4,4,5,7,9])
 
-    return child, child2
+for i in range(50):
 
-parent1 = np.array([0, 0, 1, 0, 1, 0])
-parent2 = np.array([1, 1, 1, 0, 0, 1])
+    child1, child2 = crossover(parent1, parent2)
 
-crossoverPoint = random.randint(0,5)
+    if np.random.uniform(0,1) < mutation_probability:
+        child1 = mutation(child1)
+    if np.random.uniform(0,1) < mutation_probability:
+        child2 = mutation(child2)
 
-child1 = np.concatenate((parent1[:crossoverPoint],parent2[crossoverPoint:]))
-child2 = np.concatenate((parent1[crossoverPoint:],parent2[:crossoverPoint]))
+    weight1 = np.sum(child1*weights)
+    weight2 = np.sum(child2*weights)
 
-print(child1)
-print(child2)
+    if weight1 <= 10:
+        value1 = np.sum(child1*values)
+        if value1>= best_value:
+            best_value = value1
+            best_child = child1
+
+    if weight2 <= 10:
+        value2 = np.sum(child2*values)
+        if value2>= best_value:
+            best_value = value2
+            best_child = child2
+    
+    parent1, parent2 = child1, child2
+
+    print("iteration: ",i, "best_value: ", best_value, "best_child: ", best_child)
--- Day5/Ex2.py
+++ Day5/Ex2.py
@@ -1,5 +1,16 @@
-import numpy as np
+capacity = 10
 
-weights = np.array([1, 2, 3, 4, 5, 6])
-values = np.array([2, 4, 4, 5, 7, 9])
+values = [5, 3, 2, 7, 4]
+weights = [2, 8, 4, 2, 5]
 
+test = [0, 0, 0, 0, 0]
+best = [0, 0, 0, 0, 0]
+bestVal = 0
+
+#for x in range(2**(len(values))):
+
+    #test[0] = test[0] + 1
+
+    #for y in test:
+
+        #while test[y] != 1 and test[y] != 0:
